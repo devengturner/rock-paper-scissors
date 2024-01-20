9a0b567fcdd10373f11162eb8c30ebb8be4b5a86
devengturner <devengturner93@gmail.com>
2024-01-20 15:11:48 -0500

    added a playRound function.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
 
-function getComputerChoice() {
+const computerSelection = getComputerSelection();
+const playerSelection = prompt("Enter rock, paper, or scissors.");
+
+function getComputerSelection() {
   let randomNumber = Math.trunc(Math.random() * 3) + 1;
 
   if (randomNumber == 1) {
@@ -11,3 +14,48 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+
+function playRound(computerSelection, playerSelection) {
+  const lowerCasePlayerSelection = playerSelection.toLowerCase();
+
+  if (lowerCasePlayerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "You win, paper beats rock!";
+    } else if (computerSelection === "scissors") {
+      return "You lose, scissors beats paper!";
+    } else {
+      return playRound(
+        getComputerSelection(),
+        prompt("Enter rock, paper, or scissors.")
+      );
+    }
+  }
+
+  if (lowerCasePlayerSelection === "rock") {
+    if (computerSelection === "rock") {
+      return playRound(
+        getComputerSelection(),
+        prompt("Enter rock, paper, or scissors.")
+      );
+    } else if (computerSelection === "scissors") {
+      return "You win, rock beats scissors!";
+    } else {
+      return "You lose, paper beats rock!";
+    }
+  }
+
+  if (lowerCasePlayerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return "You lose, rock beats scissors!";
+    } else if (computerSelection === "scissors") {
+      return playRound(
+        getComputerSelection(),
+        prompt("Enter rock, paper, or scissors.")
+      );
+    } else {
+      return "You win, scissors beats paper!";
+    }
+  }
+}
+
+console.log(playRound(computerSelection, playerSelection));
