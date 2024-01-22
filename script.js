eaeae19298a04b7da01d053ba2f420158fe62e41
devengturner <devengturner93@gmail.com>
2024-01-21 19:01:41 -0500

    Improve the code.

--- script.js
+++ script.js
@@ -1,97 +1,69 @@
 "use strict";
 
-const computerSelection = getComputerSelection();
-const playerSelection = prompt("Enter rock, paper, or scissors.");
+const choices = ["rock", "paper", "scissors"];
+
 let computerScore = 0;
 let playerScore = 0;
 
 function getComputerSelection() {
-  let randomNumber = Math.trunc(Math.random() * 3) + 1;
-
-  if (randomNumber == 1) {
-    return "rock";
-  } else if (randomNumber == 2) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
+  let randomNumber = Math.trunc(Math.random() * 3);
+  return choices[randomNumber];
 }
 
 function playRound(computerSelection, playerSelection) {
   const lowerCasePlayerSelection = playerSelection.toLowerCase();
 
-  if (lowerCasePlayerSelection === "paper") {
-    if (computerSelection === "rock") {
-      playerScore++;
-      return "You win, paper beats rock!";
-    } else if (computerSelection === "scissors") {
-      computerScore++;
-      return "You lose, scissors beats paper!";
-    } else {
-      return playRound(
-        getComputerSelection(),
-        prompt("Enter rock, paper, or scissors.")
-      );
-    }
+  switch (lowerCasePlayerSelection) {
+    case "rock":
+      if (computerSelection === "rock") {
+        return playRound(
+          getComputerSelection(),
+          prompt("Enter rock, paper, or scissors.")
+        );
+      } else if (computerSelection === "paper") {
+        computerScore++;
+      } else {
+        playerScore++;
+      }
+      break;
+
+    case "paper":
+      if (computerSelection === "rock") {
+        playerScore++;
+      } else if (computerSelection === "paper") {
+        return playRound(
+          getComputerSelection(),
+          prompt("Enter rock, paper, or scissors.")
+        );
+      } else {
+        computerScore++;
+      }
+      break;
+
+    case "scissors":
+      if (computerSelection === "rock") {
+        computerScore++;
+      } else if (computerSelection === "paper") {
+        playerScore++;
+      } else {
+        return playRound(
+          getComputerSelection(),
+          prompt("Enter rock, paper, or scissors.")
+        );
+      }
+      break;
   }
 
-  if (lowerCasePlayerSelection === "rock") {
-    if (computerSelection === "rock") {
-      return playRound(
-        getComputerSelection(),
-        prompt("Enter rock, paper, or scissors.")
-      );
-    } else if (computerSelection === "scissors") {
-      playerScore++;
-      return "You win, rock beats scissors!";
-    } else {
-      computerScore++;
-      return "You lose, paper beats rock!";
-    }
-  }
-
-  if (lowerCasePlayerSelection === "scissors") {
-    if (computerSelection === "rock") {
-      computerScore++;
-      return "You lose, rock beats scissors!";
-    } else if (computerSelection === "scissors") {
-      return playRound(
-        getComputerSelection(),
-        prompt("Enter rock, paper, or scissors.")
-      );
-    } else {
-      playerScore++;
-      return "You win, scissors beats paper!";
-    }
-  }
+  console.log(`Player: ${playerSelection} Computer: ${computerSelection}`);
+  console.log(`Player: ${playerScore} Computer: ${computerScore}`);
 }
 
+// playRound(computerSelection, playerSelection);
 function game() {
-  playRound(getComputerSelection(), prompt("Enter rock, paper, or scissors."));
+  const computerSelection = getComputerSelection();
+  const playerSelection = prompt("Enter rock, paper, or scissors.");
 
-  console.log(`round 1:  player: ${playerScore} computer: ${computerScore}`);
-
-  playRound(getComputerSelection(), prompt("Enter rock, paper, or scissors."));
-
-  console.log(`round 2:  player: ${playerScore} computer: ${computerScore}`);
-
-  playRound(getComputerSelection(), prompt("Enter rock, paper, or scissors."));
-
-  console.log(`round 3:  player: ${playerScore} computer: ${computerScore}`);
-
-  playRound(getComputerSelection(), prompt("Enter rock, paper, or scissors."));
-
-  console.log(`round 4:  player: ${playerScore} computer: ${computerScore}`);
-
-  playRound(getComputerSelection(), prompt("Enter rock, paper, or scissors."));
-
-  console.log(`round 5:  player: ${playerScore} computer: ${computerScore}`);
-
-  if (computerScore > playerScore) {
-    console.log("You lose!");
-  } else {
-    console.log("You win!");
-  }
+  playRound(computerSelection, playerSelection);
 }
 
 game();
